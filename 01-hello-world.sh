f2f7a1715b23059244930aa24641deadd4b37723
Narii Chowdary <nareshchowdary164@gmail.com>
2025-11-10 00:06:40 +0530

    hello world started

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,1 +1,20 @@
 #!/bin/bash
+    # ↑ This line tells the system which shell to use (bash in this case)
+
+    # This is a comment — it’s ignored by the shell
+
+    # 1️⃣ Print something
+    echo "Hello, World!"
+
+    # 2️⃣ Variables
+    name="Narri"
+    echo "Hello, $name!"
+
+    # 3️⃣ Taking user input
+    read -p "Enter your age: " age
+    echo "You are $age years old."
+
+    # 4️⃣ If condition
+    if [ $age -ge 18 ]; then
+    echo "You are an adult."
+    else

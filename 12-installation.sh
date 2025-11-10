89b868fc69955bf525d9129f627bfa5be4a30417
Narii Chowdary <nareshchowdary164@gmail.com>
2025-11-10 23:27:18 +0530

    some massage

---
+++ 12-installation.sh
@@ -0,0 +1,17 @@
+#!bin/bash
+
+USERID=$(ID -U)
+
+if [ $USERID -ne 0]; then
+    echo "ERROR:: please runthis script with root priviliage"
+    exit 1 #failure is other than 0
+fi
+
+dnf install mysql -y
+
+if [ $? -ne 0 ]; then
+    echo "ERROR:: iNSTALLING MYSQL  is failure"
+    exit 1
+else
+    echo "Installing my sql is success"
+fi

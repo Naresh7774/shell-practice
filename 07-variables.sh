17809e995ae6c7e1e32f36d272b14d3e0de2cec0
Narii Chowdary <nareshchowdary164@gmail.com>
2025-11-10 19:19:56 +0530

    some massage

--- 07-variables.sh
+++ 07-variables.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-DATE=$(date)
-echo "Timestamo exicuted : $DATE"
+#DATE=$(date)
+START_TIME=$(date +%S)
+
+sleep 10 &
+
+TOTAL_TIME=$($END_TIME-$START_TIME)
+#echo "Timestamo exicuted : $DATE"
+echo "Script executed in: $TOTAL_TIME seconds"

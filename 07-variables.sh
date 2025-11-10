1f92a165cd1ca59a6b9a35b6ee361d40efc8301b
Narii Chowdary <nareshchowdary164@gmail.com>
2025-11-10 19:21:11 +0530

    some massage

--- 07-variables.sh
+++ 07-variables.sh
@@ -5,6 +5,6 @@ START_TIME=$(date +%S)
 
 sleep 10 &
 
-TOTAL_TIME=$($END_TIME-$START_TIME)
+TOTAL_TIME=$(($END_TIME-$START_TIME))
 #echo "Timestamo exicuted : $DATE"
 echo "Script executed in: $TOTAL_TIME seconds"

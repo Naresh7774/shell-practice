422f0b5d88cc10f67e83aa378340a77899f6fdeb
Narii Chowdary <nareshchowdary164@gmail.com>
2025-11-10 19:40:39 +0530

    some massage

--- 08-special-variables.sh
+++ 08-special-variables.sh
@@ -1,3 +1,3 @@
-#echo "All variables passed to the script: $@"
-#echo "All variables passed to the script: $*"
+echo "All variables passed to the script: $@"
+echo "All variables passed to the script: $*"
 echo " Current directory : $PWD"

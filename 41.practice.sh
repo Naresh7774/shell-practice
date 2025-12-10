8f6a25a8b0c64aef79819bc79f1838334856fca2
Narii Chowdary <nareshchowdary164@gmail.com>
2025-12-10 23:24:53 +0530

    some message

---
+++ 41.practice.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#DATE=$(date)
+START_TIME=$(date +%S)
+
+sleep 10 
+
+TOTAL_TIME=$(($END_TIME-$START_TIME))
+#echo "Timestamo exicuted : $DATE"
+echo "Script executed in: $TOTAL_TIME seconds"

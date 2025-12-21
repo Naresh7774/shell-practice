1fa4aaeb4bb27b7b713bf751715853d49b0fa38e
Narii Chowdary <nareshchowdary164@gmail.com>
2025-12-21 12:50:49 +0530

    practice

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "please enter your pin number"
-read -s PIN_NUMBER #here PIN_NUMBER is variable
+read -s PIN_NUMBER #here PIN_NUMBER is variabl
 echo "PIN entered is: $PIN_NUMBER"

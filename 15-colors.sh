841cdc87e2d93e63c709896216976be2c78ef066
Narii Chowdary <nareshchowdary164@gmail.com>
2026-03-19 00:15:35 +0530

    project

--- 15-colors.sh
+++ 15-colors.sh
@@ -19,3 +19,12 @@ VALIDATE(){ # functions receive inputs through args just like shell script args
         echo -e "Installing $2 ... $G SUCCESS $N"
     fi
 }
+
+dnf list installed mysql
+# Install if it is not found
+if [ $? -ne 0 ]; then
+    dnf install mysql -y
+    VALIDATE $? "MySQL"
+else
+    echo -e "MySQL already exist ... $Y SKIPPING $N"
+fi

812fb307afed46bcdf3fae49bcf557401800eff1
Narii Chowdary <nareshchowdary164@gmail.com>
2026-03-18 23:33:22 +0530

    project

--- 03.variables.sh
+++ 03.variables.sh
@@ -1,6 +1,18 @@
-##!/bin/bash
-PERSON1=john
-PERSON2=Raheem
+# ##!/bin/bash
+# PERSON1=john
+# PERSON2=Raheem
 
-echo "Sure :: Hii suresh How are you?"
-echo "sreya:: Hello sure , I am fine. How are you?"
+# echo "Sure :: Hii suresh How are you?"
+# echo "sreya:: Hello sure , I am fine. How are you?"
+
+
+#!/bin/bash
+
+# Arguments
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: Welcome to Alaska $PERSON2"
+echo "$PERSON2:: Thank you $PERSON1, Finally not on Zoom Webinar"
+echo "$PERSON1:: So, do we have a deal"
+echo "$PERSON2:: Yes, if you make Ukraine joins Russia"

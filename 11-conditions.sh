3263cda354dc4b509c1924588062c6098278f75c
Narii Chowdary <nareshchowdary164@gmail.com>
2026-03-18 23:37:44 +0530

    project

--- 11-conditions.sh
+++ 11-conditions.sh
@@ -3,8 +3,9 @@
 echo "please enter number:"
 read NUMBER
 
+
 if [ $(($NUMBER % 2)) -eq 0 ]; then
     echo "Given number $NUMBER is EVEN"
 else
-echo "givrn number $NUMBER is odd"
+    echo "Given number $NUMBER is ODD"
 fi

aeca448c34377b0bdaeaffc04880be5d1ab0dd77
Narii Chowdary <nareshchowdary164@gmail.com>
2026-03-18 23:29:31 +0530

    project

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,26 +1,4 @@
 #!/bin/bash
-    # ↑ This line tells the system which shell to use (bash in this case)
 
-    # This is a comment — it’s ignored by the shell
-
-    # 1️⃣ Print something
-    echo "Hello, World!"
-
-    # 2️⃣ Variables
-    name="Narri"
-    echo "Hello, $name!"
-
-    # 3️⃣ Taking user input
-    read -p "Enter your age: " age
-    echo "You are $age years old."
-
-    # 4️⃣ If condition
-    if [ $age -ge 18 ]; then
-    echo "You are an adult."
-    else
-
-
-
-echo "Hello world"
-
-wddedefewwwwwww
+# this is comment, cant be executed, just for information purpose
+echo "Hello World!"

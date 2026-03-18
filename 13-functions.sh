882fe8ab5f7a9c4d299b65924782c3ff6ff88ac2
Narii Chowdary <nareshchowdary164@gmail.com>
2026-03-18 23:39:07 +0530

    project

--- 13-functions.sh
+++ 13-functions.sh
@@ -4,10 +4,10 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
     echo "ERROR:: Please run this script with root privelege"
-    exit 1 # failure is other than 0
+    exit 1 
 fi
 
-VALIDATE(){ # functions receive inputs through args just like shell script args
+VALIDATE(){ 
     if [ $1 -ne 0 ]; then
         echo "ERROR:: Installing $2 is failure"
         exit 1
@@ -18,9 +18,3 @@ VALIDATE(){ # functions receive inputs through args just like shell script args
 
 dnf install mysql -y
 VALIDATE $? "MySQL"
-
-dnf install nginx -y
-VALIDATE $? "Nginx"
-
-dnf install python3 -y
-VALIDATE $? "python3"
